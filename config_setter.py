7031e1df5b4b5829b4811baa9bece87e8b8a0f80
josh-laptop <rcxwhiz@gmail.com>
2019-12-12 10:34:52 -0700

    Added checker class and logging.

--- config_setter.py
+++ config_setter.py
@@ -7,6 +7,7 @@ config.read(CONFIG_NAME)
 status_url = config.get('setup', 'status_url')
 phone_number = config.get('setup', 'status_url')
 check_interval = config.getint('setup', 'check_interval')
+logging_level = config.get('setup', 'logging_level')
 
 fails_required = config.getint('preferrences', 'fails_required')
 text_interval = config.getfloat('preferrences', 'text_interval')
---
+++ server_checker/checker.py
@@ -0,0 +1,10 @@
+import config_setter as cfg
+import logging
+import coloredlogs
+coloredlogs.install(level=cfg.logging_level, ftm='%(asctime)s %(levelname)s %message)s')
+
+
+class Checker:
+
+	def __init__(self):
+		logging.debug('Initializing checker')
