386fb7f1762f61b0c56a35faef6030f3eb90370a
josh-laptop <rcxwhiz@gmail.com>
2019-12-12 12:37:29 -0700

    Additions to looping members.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from server_checker import *
 logging.info('Starting server down notifier program')
-checker = Checker()
 
-# stuff
+checker = Checker()
 
 logging.info('Exiting server down notifier')
--- server_checker/checker.py
+++ server_checker/checker.py
@@ -11,16 +11,55 @@ class Checker:
 
 	def __init__(self):
 		logging.info('Initializing checker')
-		self.failed = False
-		self.up_loop()
+		if self.check_server_up():
+			self.up_loop()
+		else:
+			self.down_loop()
+
+	def check_server_up(self):
+		# TODO this is going to use webscraping to get the server status
+		return True
 
 	def up_loop(self):
-		time_since_message = 0
+		player_list = []
+		time_since_message = cfg.up_text_interval + 1
+		uptime = 0
+
 		while True:
+			if not self.check_server_up():
+				self.down_loop()
+				return None
+
+			if (not cfg.up_text_interval == 0) and (time_since_message > cfg.up_text_interval):
+				self.send_up_message(uptime, player_list)
+				time_since_message = 0
+
+			# TODO use some webscraping to append to player_list
+
 			time.sleep(cfg.check_interval * 60)
 			time_since_message += cfg.check_interval / 60
-			if not cfg.up_text_interval == 0 and time_since_message > cfg.up_text_interval:
-				self.send_up_message()
 
-	def send_up_message(self):
-		logging.warning("I'm supposed to make this text me with info")
+	def down_loop(self):
+		time_since_message = cfg.down_text_interval + 1
+		downtime = 0
+
+		while True:
+			if self.check_server_up():
+				self.up_loop()
+				return None
+
+			if time_since_message > cfg.down_text_interval:
+				self.send_down_message(downtime)
+				time_since_message = 0
+
+			time.sleep(cfg.check_interval * 60)
+			if cfg.down_text_interval > 0:
+				time_since_message += cfg.check_interval / 60
+
+	def send_up_message(self, uptime, player_list):
+		logging.warning("The server is up???")
+		# TODO make this send as a text
+
+	def send_down_message(self, downtime):
+		logging.warning("The server is down???")
+		# TODO make this send as a text
